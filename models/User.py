9c082711dde0665162dd82c7d44c6d67bac3ec82
probrahim <ibrahimhassani674@gmail.com>
2025-04-01 18:48:30 +0000

    class User

---
+++ models/User.py
@@ -0,0 +1,14 @@
+#!/bin/bash/python3
+
+
+class Utilisateur():
+	# constructeur 
+	def __init(self,uid,name,age,email):
+		self.id = uid
+		self.name = name
+		self.age = age
+		self.email = email
+	# to string
+	def __str__(self)
+		return f" id :{self.id} , name : {self.name}, age = {self.age} ,email : {self.email}" 
+
